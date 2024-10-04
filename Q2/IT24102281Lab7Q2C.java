8cad7ceee6d51a65652738ba04c996575eb86db3
it24102281y <it24102281@my.sliit.lk>
2024-10-04 09:06:07 +0530

    IT24102281Lab7Q2C

---
+++ Q2/IT24102281Lab7Q2C.java
@@ -0,0 +1,15 @@
+public class IT24100068Lab7Q2C
+{
+	public static void main(String[] args) 
+	{
+		int x, y;
+		for(x = 5; x > 0; x=x-1)
+		{
+			for(y = 1; y <= x; y=y+1)
+			{
+				System.out.print(x+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
