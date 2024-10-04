e6290a97e0fa03bc8b3e61aa0bc24882a9aa8096
it24102281y <it24102281@my.sliit.lk>
2024-10-04 08:59:18 +0530

    IT24102281Lab7QA

---
+++ Q2/IT24102281Lab7Q2A.java.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IT24102281Lab7Q2A
+{
+	public static void main(String[] args) 
+	{
+		int x, y;
+		for(x = 1; x < 6; x=x+1)
+		{
+			for(y = 1; y < 6; y=y+1)
+			{
+				System.out.print("$ ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
